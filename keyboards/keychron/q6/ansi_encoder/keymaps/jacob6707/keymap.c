e7addeb79c20ad72d87e04c49e7d346350eafd2e
jacob6707 <22101656+jacob6707@users.noreply.github.com>
2025-02-10 00:59:37 +0100

    fix keymap

--- keyboards/keychron/q6/ansi_encoder/keymaps/jacob6707/keymap.c
+++ keyboards/keychron/q6/ansi_encoder/keymaps/jacob6707/keymap.c
@@ -51,7 +51,6 @@ bool isIdle = true;
 static bool is_volume_display_active = false;
 static uint32_t last_volume_change_time = 0;    // Time of the last volume change
 static uint8_t volume_percentage = 0;           // Last received volume percentage
-
 static uint8_t max_x_coord = 0;                 // To store the rightmost x-coordinate
 
 #define KC_WINSS KC_WINDOWS_SCREENSHOT
@@ -96,6 +95,21 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 };
 #endif
 
+#if !defined(RGB_MATRIX_ENABLE) || (!defined(CAPS_LOCK_LED_INDEX) && !defined(NUM_LOCK_LED_INDEX))
+    bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
+        if (!process_record_user(keycode,record)) {
+            return false;
+        }
+        return true;
+    }
+    bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max) {
+        if (!rgb_matrix_indicators_advanced_user(led_min, led_max)) {
+            return false;
+        }
+        return true;
+    }
+#endif
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (isIdle && record->event.pressed) {
         isIdle = false;
@@ -111,6 +125,38 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 unregister_code(KC_S);
                 unregister_code(KC_LSFT);
                 unregister_code(KC_LWIN);
+            }
+            return false;
+        case KC_CAPS_LOCK:
+            if (record->event.pressed) {
+                if (!host_keyboard_led_state().caps_lock) {
+                    if (rgb_matrix_get_flags() == LED_FLAG_ALL) {
+                        rgb_matrix_sethsv_noeeprom(HSV_RED);
+                    }
+                } else {
+                    rgb_matrix_reload_from_eeprom();
+                }
+            }
+            return true;
+        case QK_RGB_MATRIX_TOGGLE:
+        case RGB_TOG:
+            if (record->event.pressed) {
+                switch (rgb_matrix_get_flags()) {
+                    case LED_FLAG_ALL: {
+                        if (host_keyboard_led_state().caps_lock) {
+                            rgb_matrix_reload_from_eeprom();
+                        }
+                        rgb_matrix_set_flags(LED_FLAG_NONE);
+                        rgb_matrix_set_color_all(0, 0, 0);
+                    } break;
+                    default: {
+                        rgb_matrix_set_flags(LED_FLAG_ALL);
+                    } break;
+                }
+            }
+            if (!rgb_matrix_is_enabled()) {
+                rgb_matrix_set_flags(LED_FLAG_ALL);
+                rgb_matrix_enable();
             }
             return false;
     }
@@ -171,27 +217,47 @@ bool via_command_kb(uint8_t *data, uint8_t length) {
 
 // Custom RGB Matrix effect
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-    if (!is_volume_display_active) return true;
+    if (is_volume_display_active) {
+        uint8_t threshold_x = (max_x_coord * volume_percentage) / 100;
+        uint16_t fade_time = map_to_time_range(g_custom_config.vol_conf.fade_speed);
 
-    uint8_t threshold_x = (max_x_coord * volume_percentage) / 100;
-    uint16_t fade_time = map_to_time_range(g_custom_config.vol_conf.fade_speed);
+        uint8_t brightness = get_fadeout_value((1.0f - timer_elapsed(last_volume_change_time) / (float)fade_time) * 255);
+        RGB rgb = hsv_to_rgb(g_custom_config.vol_conf.hsv);
 
-    uint8_t brightness = get_fadeout_value((1.0f - timer_elapsed(last_volume_change_time) / (float)fade_time) * 255);
-    RGB rgb = hsv_to_rgb(g_custom_config.vol_conf.hsv);
+        for (uint8_t i = led_min; i < led_max; i++) {
+            if (g_led_config.point[i].x < threshold_x) {
+                rgb_matrix_set_color(i, rgb.r * brightness / 255, rgb.g * brightness / 255, rgb.b * brightness / 255);  // Light up LEDs within the threshold
+            }
+            else if (g_led_config.point[i].x <= threshold_x + 16) {
+                uint8_t ratio = (threshold_x + 16 - g_led_config.point[i].x) * 255 / 16;
+                uint8_t fade_value = (brightness * ratio) / 255;
 
-    for (uint8_t i = led_min; i < led_max; i++) {
-        if (g_led_config.point[i].x < threshold_x) {
-            rgb_matrix_set_color(i, rgb.r * brightness / 255, rgb.g * brightness / 255, rgb.b * brightness / 255);  // Light up LEDs within the threshold
-        }
-        else if (g_led_config.point[i].x <= threshold_x + 16) {
-            uint8_t ratio = (threshold_x + 16 - g_led_config.point[i].x) * 255 / 16;
-            uint8_t fade_value = (brightness * ratio) / 255;
-
-            rgb_matrix_set_color(i, rgb.r * fade_value / 255, rgb.g * fade_value / 255, rgb.b * fade_value / 255);  // Light up the LED at the threshold
-        } else {
-            rgb_matrix_set_color(i, 0, 0, 0);
+                rgb_matrix_set_color(i, rgb.r * fade_value / 255, rgb.g * fade_value / 255, rgb.b * fade_value / 255);  // Light up the LED at the threshold
+            } else {
+                rgb_matrix_set_color(i, 0, 0, 0);
+            }
         }
     }
+
+    // RGB_MATRIX_INDICATOR_SET_COLOR(index, red, green, blue);
+#   if defined(CAPS_LOCK_LED)
+    if (host_keyboard_led_state().caps_lock) {
+        RGB_MATRIX_INDICATOR_SET_COLOR(CAPS_LOCK_LED, 255, 0, 0);
+    } else {
+        if (!rgb_matrix_get_flags()) {
+            RGB_MATRIX_INDICATOR_SET_COLOR(CAPS_LOCK_LED, 0, 0, 0);
+        }
+    }
+#   endif // CAPS_LOCK_LED
+#   if defined(NUM_LOCK_LED)
+    if (host_keyboard_led_state().num_lock) {
+        RGB_MATRIX_INDICATOR_SET_COLOR(NUM_LOCK_LED, 0, 255, 0);
+    } else {
+        if (!rgb_matrix_get_flags()) {
+            RGB_MATRIX_INDICATOR_SET_COLOR(NUM_LOCK_LED, 0, 0, 0);
+        }
+    }
+#   endif // NUM_LOCK_LED
 
     return true; // Return true to allow other effects to run
 }
